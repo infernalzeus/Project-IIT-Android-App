e12c654c1f9d6fec5b5684c766638f8183ccb5f1
infernalzeus <46246206+infernalzeus@users.noreply.github.com>
2020-07-08 19:14:53 +0530

    Create vidactivity.java

---
+++ ProjectIITApp/app/src/main/java/com/example/projectiitapp/vidactivity.java
@@ -0,0 +1,52 @@
+
+package com.example.projectiitapp;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Config;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+public class vidactivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    YouTubePlayerView youTubePlayerView;
+    String API_KEY="AIzaSyDue67OKtEWbcTC9H24wPkDKvyDQ_Fxu1Q";
+    private static final int RECOVERY_REQUEST = 1;
+    String TAG="vidactivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vidactivity);
+        youTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtubeview);
+        youTubePlayerView.initialize(API_KEY, this);
+    }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        Bundle bundle = getIntent().getExtras();
+        String showVideo = bundle.getString("videoId");
+
+        Log.e(TAG,"Video" +showVideo);
+        youTubePlayer.cueVideo(showVideo);
+    }
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            Toast.makeText(this, "Error Initializing Youtube Player", Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            getYouTubePlayerProvider().initialize(API_KEY, this);
+        }
+    }
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubePlayerView;
+    }
+}
